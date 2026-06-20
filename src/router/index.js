7e722f5253389019ce7e57466e244b94ca2ce5de
protonpixels <primelaunch34v@proton.me>
2026-06-19 21:10:48 -0400

    'router'

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import ScoutingForm from "@/components/ScoutingForm.vue";
-import UFCPredictor from "@/components/UFCPredictor.vue";
 import FightForm from "../components/FightForm.vue";
 
-const routes = [
-  { path: "/", name: "home", component: ScoutingForm },
-  { path: "/football", name: "football-scout", component: ScoutingForm },
-  { path: "/mma", name: "mma-analysis", component: FightForm },
-];
+const routes = [{ path: "/", name: "home", component: FightForm }];
 
 export default createRouter({
   // Hash history keeps it working when opened from a static file / preview.
